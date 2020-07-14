64625ec22bbc0b5fbc3f2eee6bc2e45c56cd92f3
Sohail Aj <sohail.aj05@gmail.com>
2020-07-14 13:55:19 -0400

    Add  listVersions, rollback, getTemplateAtVersion
    Fix 'FirebaseRemoteConfigService incorrectly implements interface 'RemoteConfig'.

--- lib/services/firebase-admin-remote-config.service.ts
+++ lib/services/firebase-admin-remote-config.service.ts
@@ -15,18 +15,34 @@ export class FirebaseRemoteConfigService implements admin.remoteConfig.RemoteCon
   getTemplate(): Promise<admin.remoteConfig.RemoteConfigTemplate> {
     return this.remoteConfig.getTemplate();
   }
+
+  getTemplateAtVersion(versionNumber: number | string): Promise<admin.remoteConfig.RemoteConfigTemplate> {
+    return this.remoteConfig.getTemplateAtVersion(versionNumber);
+  }
+
   validateTemplate(
     template: admin.remoteConfig.RemoteConfigTemplate,
   ): Promise<admin.remoteConfig.RemoteConfigTemplate> {
     return this.remoteConfig.validateTemplate(template);
   }
+
   publishTemplate(
     template: admin.remoteConfig.RemoteConfigTemplate,
     options?: { force: boolean },
   ): Promise<admin.remoteConfig.RemoteConfigTemplate> {
     return this.remoteConfig.publishTemplate(template, options);
   }
+  
   createTemplateFromJSON(json: string): admin.remoteConfig.RemoteConfigTemplate {
     return this.remoteConfig.createTemplateFromJSON(json);
   }
+
+  rollback(versionNumber: string | number): Promise<admin.remoteConfig.RemoteConfigTemplate> {
+    return this.remoteConfig.rollback(versionNumber)
+  }
+
+  listVersions(options?: admin.remoteConfig.ListVersionsOptions): Promise<admin.remoteConfig.ListVersionsResult> {
+    return this.remoteConfig.listVersions(options)
+  }
+  
 }
